4ce6d7917dcddc2a4cd15414b59734c9e5d9f864
Jean Carlos Meninno <eth0.lo@gmail.com>
2013-03-04 20:14:04 +0100

    adding docco to the project

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,12 +2,16 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-
-
   grunt.initConfig({
+    'clean': {
+      'tests': {
+        'src': ['app/public/js/tests', 'app/public/js/src']
+      }
+    },
     'connect': {
       'server': {
         'options': {
@@ -23,9 +27,10 @@ module.exports = function(grunt) {
                   {'expand': true, 'cwd': 'src/', 'src': ['**'], 'dest': 'app/public/js/src'}]
       }
     },
-    'clean': {
-      'tests': {
-        'src': ['app/public/js/tests', 'app/public/js/src']
+    "docco": {
+      "src": ['src/*.js'],
+      "options": {
+        "output": 'docs/'
       }
     },
     'jshint': {
